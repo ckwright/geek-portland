636d2fa86ac7d8184211f494c8f40cba8a1440e8
Maria Fisher <mariacha@gmail.com>
2014-01-18 17:50:45 -0800

    Changing the panel's border shadow to be less IE8 supportive.

---
+++ my_profile/themes/custom/geek_portland/templates/panels-rounded-corners-box.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ *
+ * Display the box for rounded corners.
+ *
+ * - $content: The content of the box.
+ */
+?>
+<div class="rounded-corner-panel">
+  <?php print $content; ?>
+</div>
