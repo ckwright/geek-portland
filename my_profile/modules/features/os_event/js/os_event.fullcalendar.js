739d6ef4770a22be84eade047979933f61dfcc9f
Maria Fisher <mariacha@gmail.com>
2014-04-06 19:10:58 -0700

    Adding cookie to save user selected date.

--- my_profile/modules/features/os_event/js/os_event.fullcalendar.js
+++ my_profile/modules/features/os_event/js/os_event.fullcalendar.js
@@ -6,7 +6,7 @@
 (function($) {
 Drupal.fullcalendar.plugins.os_event = {
   options: function (fullcalendar, settings) {
-    return {
+    var time_options = {
       eventClick: function(calEvent, jsEvent, view) {
         if (settings.sameWindow) {
           window.open(calEvent.url, '_self');
@@ -15,8 +15,23 @@ Drupal.fullcalendar.plugins.os_event = {
           window.open(calEvent.url + '?popup', '_blank', 'height=500,width=600,scrollbars=yes');
         }
         return false;
-      }
+      },
+      viewRender: function(view, fullcalendar) {
+        var view_start_date = view.start;
+        var view_start = {
+          day : view_start_date.getDay(),
+          month : view_start_date.getMonth(),
+          year : view_start_date.getFullYear(),
+        }
+        $.cookie('fullcalendar_start', JSON.stringify(view_start), {json:true});
+      },
     };
+
+    if ($.cookie('fullcalendar_start')) {
+      time_options = $.extend({}, JSON.parse($.cookie('fullcalendar_start')), time_options);
+    }
+
+    return time_options;
   }
 };
 }(jQuery));
