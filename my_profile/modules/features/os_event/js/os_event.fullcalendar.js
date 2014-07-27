516320333bf1a55758a5801e69a7ed55503a253c
Maria Fisher <mariacha@gmail.com>
2014-07-27 14:13:12 -0700

    First attempt at striping.

--- my_profile/modules/features/os_event/js/os_event.fullcalendar.js
+++ my_profile/modules/features/os_event/js/os_event.fullcalendar.js
@@ -15,8 +15,26 @@ Drupal.fullcalendar.plugins.os_event = {
           window.open(calEvent.url + '?popup', '_blank', 'height=500,width=600,scrollbars=yes');
         }
         return false;
+      },
+      eventAfterAllRender: function ( view ) {
+        $('.fc-day-content').each(function() {
+          var top = $(this).offset().top;
+          var left = $(this).offset().left;
+          var bottom = top + $(this).height();
+          var right = left + $(this).width();
+
+          var todayEvents = $(".fc-event").filter(function(i) {
+            return
+              $(this).offset().top >= top
+              && $(this).offset().top < bottom
+              && $(this).offset().left >= left
+              && $(this).offset().left < right;
+          });
+
+          todayEvents.css('background', '#000');
+        });
       }
-    };
+    }
   }
-};
+}
 }(jQuery));
