4d6e7e4b01a8698f20010773a9de098c5c84fa74
Maria Fisher <mariacha@gmail.com>
2014-10-06 18:53:03 -0700

    Initial addition of fullcalandar library.

---
+++ my_profile/modules/custom/geek_fullcalendar/js/geek_fullcalendar.fullcalendar.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * Changes onclick behavior for fullcalendar module to open a popup.
+ */
+
+(function($) {
+  Drupal.behaviors.fullcalendar = {
+    attach: function (context, settings) {
+      $('#calendar').fullCalendar({
+        header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+        },
+        events: {
+          url: 'events.json',
+          error: function() {
+            $('#script-warning').show();
+          }
+        },
+        loading: function(bool) {
+          $('#loading').toggle(bool);
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+          if (settings.sameWindow) {
+            window.open(calEvent.url, '_self');
+          }
+          else {
+            window.open(calEvent.url + '?popup', '_blank', 'height=500,width=600,scrollbars=yes');
+          }
+          return false;
+        },
+        eventAfterAllRender: function ( view ) {
+          var eventsPerWeekDay = {};
+          var arrayKeys = [];
+
+          $(".fc-event").each(function(i){
+            var keyValue = $(this).prop('offsetTop') + ($(this).prop('offsetLeft') * 100);
+            eventsPerWeekDay[keyValue] = $(this);
+            arrayKeys.push(keyValue);
+          });
+
+          arrayKeys.sort();
+
+          for(var i=0; i < arrayKeys.length; i++) {
+            if (i % 2 === 1) {
+              eventsPerWeekDay[arrayKeys[i]].addClass('fc-event-odd');
+            }
+          };
+        },
+      });
+    }
+  };
+}(jQuery));
