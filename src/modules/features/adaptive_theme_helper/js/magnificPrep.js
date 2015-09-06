9d62df0904c7268d104743e3cc255431a65990be
Maria Fisher <mariacha@gmail.com>
2015-09-06 13:19:20 -0700

    Linting js file magnificPrep.js.

--- src/modules/features/adaptive_theme_helper/js/magnificPrep.js
+++ src/modules/features/adaptive_theme_helper/js/magnificPrep.js
@@ -12,7 +12,7 @@
         gallery: {
           enabled: true,
           navigateByImgClick: true,
-          preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+          preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
           tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
